178621a4d81af67aee4a03489c231a15bd99c44f
TheSamuelVitor <thesamuelvitor.py@gmail.com>
2022-09-12 23:04:52 -0300

    :lipstick: working with components and properties

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -6,23 +6,37 @@
   - Devem vir com a letra maiscula;
   - Devem ser uma função;
   - No final deve ser exportado como default;
+  - São as tags HTML;
+
+  Propriedades:
+  - Atributos de um componente.
 */
 
-function Botao2() {
+// Criando a interface que será usada
+interface ButtonProps {
+  id: number
+  title: string
+}
+
+{/* 
+  Colchetes são necessarios para fazer o data binding
+*/}
+function Botao2(props: ButtonProps) {
   return (
     <button>
-      Send
+      {props.id} {props.title}
     </button>
   )
 }
 
+// Passando as propriedades passadas anteriormente na interface
 function App() {
   return (
     <>
       <h1>Hello World</h1>
-      <Botao2 />
-      <Botao2 />
-      <Botao2 />
+      <Botao2 id={1} title="Send" />
+      <Botao2 id={2} title="Send" />
+      <Botao2 id={3} title="Send" />
     </>
   )
 }
