1ab27a4aee7efede399c37cbb8673a220244576c
TheSamuelVitor <thesamuelvitor.py@gmail.com>
2022-10-22 22:12:58 -0300

    feat: adding new routes on server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,66 +1,97 @@
-// importando uma dependencia
 
-// -- de uma forma antiga
-// o arquivo pode ser apenas ".js"
-// const express = require('express');
+import express, { json, request, response } from "express";
+import { PrismaClient } from "@prisma/client";
+import { convertHourStringToMinute } from "./utils/convert-hour-minute";
 
-// -- de uma forma mais nova
-// ECMAScript Modules
-// o arquivo deve ser ".mjs"
-import express, { request, response } from "express";
-
-// inicializando o app com express
 const app = express();
 
-app.get("/games", (request, response) => {
-  return response.status(201).json([
-    {
-      id: 1,
-      name: "CS: GO",
-    },
-    {
-      id: 2,
-      name: "Lol",
-    },
-    {
-      id: 3,
-      name: "Valorant",
-    },
-    {
-      id: 4,
-      name: "GTA Vice City",
-    },
-  ]);
+app.use(express.json())
+
+const prisma = new PrismaClient({
+  log: ["query"],
 });
 
-app.get("/ads", (request, response) => {
-  console.log("Acessou /ads");
-  return response.status(200).json([
-    {
-      id: 1,
-      name: "Anuncio 1",
+app.get("/games", async (request, response) => {
+  const games = await prisma.game.findMany({
+    include: {
+      _count: {
+        select: {
+          ads: true,
+        },
+      },
     },
-    {
-      id: 2,
-      name: "Anuncio 2",
-    },
-    {
-      id: 3,
-      name: "Anuncio 3",
-    },
-    {
-      id: 4,
-      name: "Anuncio 4",
-    },
-  ]);
+  });
+
+  return response.status(201).json(games);
 });
 
-app.post("/ads", (request, response) => {
-  return response.status(202).json([
-    {
-      message: "created",
+app.get("/games/:id/ads", async (request, response) => {
+  const gameId = request.params.id;
+
+  const ads = await prisma.ad.findMany({
+    select: {
+      id: true,
+      name: true,
+      weekDays: true,
+      useVoiceChannel: true,
+      yearsPlaying: true,
+      hourStart: true,
+      hourEnd: true,
     },
-  ]);
+    where: {
+      gameId: gameId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  return response.json(
+    ads.map((ad) => {
+      return {
+        ...ad,
+        weekDays: ad.weekDays.split(",")
+      };
+    })
+  );
+});
+
+app.get("/ads/:id/discord", async (request, response) => {
+  const adId = request.params.id
+
+  const ad = await prisma.ad.findUniqueOrThrow({
+    select: {
+      discord: true,
+    }, 
+    where: {
+      id: adId
+    }
+  })
+
+  return response.json({
+    discord: ad.discord
+  })
+});
+
+app.post("/games/:id/ads", async (request, response) => {
+  const gameId = request.params.id
+  const body = request.body
+
+
+  const ad = await prisma.ad.create({
+    data: {
+      gameId,
+      name: body.name,
+      yearsPlaying: body.yearsPlaying,
+      discord: body.discord,
+      weekDays: body.weekDays.join(','),
+      hourStart: convertHourStringToMinute(body.hourStart),
+      hourEnd: convertHourStringToMinute(body.hourEnd),
+      useVoiceChannel: body.useVoiceChannel
+    }
+  })
+
+  return response.status(202).json(ad)
 });
 
 // passando a rota para o aplicativo rodar
